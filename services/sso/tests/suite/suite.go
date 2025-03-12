94b5bee047937f46f3346c2eb524bc781e372ac8
KordyD <84672403+KordyD@users.noreply.github.com>
2025-03-12 13:44:57 +0300

    add .gitignore

--- services/sso/tests/suite/suite.go
+++ services/sso/tests/suite/suite.go
@@ -19,7 +19,7 @@ type Suite struct {
 	Cfg        *config.Config
 }
 
-const configPath = "../../config/config.yml"
+const configPath = "../../config/config.yaml"
 
 func New(t *testing.T) (*Suite, context.Context) {
 	t.Helper()
